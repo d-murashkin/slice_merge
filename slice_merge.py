9354b593f56691d3c38dd6b705e3a97b71cc1ed7
Dmitrii Murashkin <murashkin@uni-bremen.de>
2020-01-15 19:45:25 +0100

    [new] __init__ for TiledImage

--- slice_merge.py
+++ slice_merge.py
@@ -2,6 +2,67 @@
 A tool to slice large images into a set of small ones.
 And then to merge them back into the image of the original size.
 """
+import numpy as np
+
+
+class TiledImage(object):
+    """ Class contains the image, information about the split and a list of tiles.
+        Either tile size or number_of_tiles should be specified.
+        Both options can be integers or tuples.
+        keep_rest option defines weather tiles of a smaller size should be kept.
+        *channels* can be 'last' if channes of the image is the last index or 'first' otherwise.
+    """
+    def __init__(self, image, tile_size=0, number_of_tiles=0, keep_rest=True, channels='last'):
+        self.image = image
+        self.keep_rest = keep_rest
+        self.channels = channels
+
+        """ Dimentions of the input image """
+        if len(image.shape) == 2:
+            X, Y = image.shape
+        elif len(image.shape) == 3:
+            if channels.lower() == 'last':
+                X, Y, _ = image.shape
+            elif channels.lower() == 'first':
+                _, X, Y = image.shape
+            else:
+                print('Unknown option {1} for *channels*.'.format(channels))
+                return False
+        else:
+            print('Image has less than 2 dimentions or more than 3.')
+            print('Currently such images are not supported.')
+            return False
+
+        """ Set tile width and hight """
+        if tile_size:
+            try:
+                X_sub, Y_sub = tile_size
+            except:
+                X_sub = Y_sub = tile_size
+        elif number_of_tiles == 0:
+            print('Either tile_size or number_of_tiles should be specified.')
+            return False
+        else:
+            try:
+                X_num, Y_num = number_of_tiles
+            except:
+                X_num = Y_num = number_of_tiles
+            X_sub = X // X_num
+            Y_sub = Y // Y_num
+
+        """ Define grid """
+        if (not keep_rest) or (X % X_sub == 0):
+            xs = np.arange(0, X, X_sub)
+        else:
+            xs = np.arange(0, X + X_sub, X_sub)
+        
+        if (not keep_rest) or (Y % Y_sub == 0):
+            ys = np.arange(0, Y, Y_sub)
+        else:
+            ys = np.arange(0, Y + Y_sub, Y_sub)
+        
+        self.coords = [{'x_min': x, 'x_max': x + X_sub, 'y_min': y, 'y_max': y + Y_sub, 'subimage_location': (i, j)} for i, x in enumerate(xs) for j, y in enumerate(ys)]
+                
 
 if __name__ == '__main__':
     pass
