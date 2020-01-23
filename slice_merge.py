2d5dce462e26506614f78083f52b834d5f848f1e
Dmitrii Murashkin <murdm@ztm82.math.uni-bremen.de>
2020-01-23 10:38:38 +0100

    merge function

--- slice_merge.py
+++ slice_merge.py
@@ -45,21 +45,6 @@ class TiledImage(object):
             self.X_sub = self.X // self.X_num
             self.Y_sub = self.Y // self.Y_num
 
-        """ Define grid """
-        '''
-        if (not keep_rest) or (self.X % self.X_sub == 0):
-            xs = np.arange(0, self.X, self.X_sub)
-        else:
-            xs = np.arange(0, self.X + self.X_sub, self.X_sub)
-        
-        if (not keep_rest) or (self.Y % self.Y_sub == 0):
-            ys = np.arange(0, self.Y, self.Y_sub)
-        else:
-            ys = np.arange(0, self.Y + self.Y_sub, self.Y_sub)
-        
-        self.coords = [{'x_min': x, 'x_max': x + self.X_sub, 'y_min': y, 'y_max': y + self.Y_sub, 'subimage_location': (i, j)} for i, x in enumerate(xs) for j, y in enumerate(ys)]
-        '''
-
         """ Represent the image as an 5d array """
         image_eq_div = np.zeros((self.X_sub * self.X_num, self.Y_sub * self.Y_num, self.Z), dtype=image.dtype)
         image_eq_div[:self.X, :self.Y, :] = image_copy
@@ -67,26 +52,19 @@ class TiledImage(object):
         print(self.X_num, self.Y_num)
         self.data = np.array([np.hsplit(item, self.Y_num) for item in np.vsplit(image_eq_div, self.X_num)])
     
-    '''
-    def split(self):
-        """ Split the image into tiles.
-            The list of tiles is available at self.tiles
-        """
-        self.tiles = [Tile(self.image, item['x_min'], item['x_max'], item['y_min'], item['y_max']) for item in self.coords]
-
-    def merge(self):
-        """ Merge data from tiles into the large image.
-        """
-        self.output = np.zeros_like(self.image)
-        for tile in self.tiles:
-            self.output[tile.x_min:tile.x_max, tile.y_min:tile.y_max] = tile.image
-    '''
     def list_tiles(self):
         i, j, X, Y, Z = self.data.shape
         return np.reshape(self.data, (i * j, X, Y, Z))
     
+    def merge(self, data):
+        """ Merge the *data* 5D array with results into a 3D array image size of the original image.
+        """
+        return np.hstack(np.hstack(data))[:self.X, :self.Y, :]
+
     def image(self):
-        return np.hstack(np.hstack(self.data))[:self.X, :self.Y, :]
+        """ Return the original 3D array.
+        """
+        return self.merge(self.data)
     
     def apply(self, fucntion, parallel=False):
         """ Apply the specified function to each tile.
@@ -119,16 +97,7 @@ class TiledImage(object):
         except:
             print('Something went wrong...')
             return False
-    
 
-class Tile(object):
-    def __init__(self, image, x_min, x_max, y_min, y_max):
-        self.x_min = x_min
-        self.x_max = x_max
-        self.y_min = y_min
-        self.y_max = y_max
-        self.image = image[x_min:x_max, y_min:y_max]
-                
 
 if __name__ == '__main__':
     pass
